f23d24bed21d59daf5f633288e748dccd30ea20b
FrancescoPeriti <peritifrancesco@gmail.com>
2018-11-02 14:07:59 +0100

    Aggiunte immagini ai personaggi - da rivedere

---
+++ DataManagmentDocument/dmDocument.sh
@@ -0,0 +1,6 @@
+pdflatex dmDocument.tex;
+rm dmDocument.log;
+rm dmDocument.aux ;
+rm dmDocument.out ;
+rm dmDocument.toc;
+xdg-open dmDocument.pdf;
---
+++ LevelDesignDocument/ldDocument.sh
@@ -0,0 +1,6 @@
+pdflatex ldDocument.tex;
+rm ldDocument.log;
+rm ldDocument.aux ;
+rm ldDocument.out ;
+rm ldDocument.toc;
+xdg-open ldDocument.pdf;
